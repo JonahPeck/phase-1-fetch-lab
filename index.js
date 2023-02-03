b12a349d3b5a5017efc542de72db70f360371da5
JonahPeck <PeckJonahC@gmail.com>
2023-02-03 09:24:36 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-function fetchBooks() {
+function fetchBooks (){
+return fetch("https://anapioficeandfire.com/api/books")
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  .then((response) => response.json())
+  .then((json) => renderBooks(json))  
+  }
+  fetchBooks();
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
